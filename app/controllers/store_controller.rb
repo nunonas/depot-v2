f2b5c65eda1b34d5f71879c3d9aa4cd051491e96
nunonas <nuno_nas@netcabo.pt>
2017-12-29 19:48:59 +0000

    moved cart to sidebar

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
-  def index
-  	@products = Product.order(:title)
-  end
+
+	include CurrentCart 
+	before_action :set_cart
+
+	def index
+		@products = Product.order(:title)
+	end
 end
