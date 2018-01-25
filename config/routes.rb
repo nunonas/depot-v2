3cd2278ead0ca81a39bdf654be7a5b913d456599
nunonas <nuno_nas@netcabo.pt>
2018-01-25 16:00:49 +0000

    selecting locales added

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,24 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  get 'store/index'
+  #resources :orders
+  #resources :line_items
+  #resources :carts
+  #get 'store/index'
 
   resources :products do
   	get :who_bought, on: :member
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+  	resources :orders
+  	resources :line_items
+  	resources :carts
+  	root 'store#index', as: 'store'
+  end
+
+
+
+  #root 'store#index', as: 'store'
 end
